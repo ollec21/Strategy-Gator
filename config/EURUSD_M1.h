ecbd07a90c285c4323af4ec1ecbea1fe2db0e376
kenorb <kenorb@users.noreply.github.com>
2021-01-17 06:13:55 +0000

    Optimize Indicator params (pf=0.00; dd=0.00; tt=0)

--- config/EURUSD_M1.h
+++ config/EURUSD_M1.h
@@ -1,4 +1,4 @@
-/*
+/**
  * @file
  * Defines default strategy parameter values for the given timeframe.
  */
@@ -7,14 +7,14 @@
 struct Indi_Gator_Params_M1 : GatorParams {
   Indi_Gator_Params_M1() : GatorParams(indi_gator_defaults, PERIOD_M1) {
     applied_price = (ENUM_APPLIED_PRICE)0;
-    jaw_period = 30;
-    jaw_shift = 0;
-    teeth_period = 10;
-    teeth_shift = 11;
-    lips_period = 34;
-    lips_shift = 0;
-    ma_method = (ENUM_MA_METHOD)2;
+    ma_method = (ENUM_MA_METHOD)0;
+    period_jaw = 4;
+    period_lips = 4;
+    period_teeth = 4;
     shift = 0;
+    shift_jaw = 0;
+    shift_lips = 0;
+    shift_teeth = 0;
   }
 } indi_gator_m1;
 
@@ -25,12 +25,12 @@ struct Stg_Gator_Params_M1 : StgParams {
     lot_size = 0;
     signal_open_method = 0;
     signal_open_filter = 1;
-    signal_open_level = (float)0;
+    signal_open_level = (float)0.0;
     signal_open_boost = 0;
     signal_close_method = 0;
     signal_close_level = (float)0;
     price_stop_method = 0;
-    price_stop_level = (float)2;
+    price_stop_level = (float)1;
     tick_filter_method = 1;
     max_spread = 0;
   }
